f5a1a51b5d06a189e441046a0dd6ccc1ed9f4a86
EC2 Default User <ec2-user@ip-172-31-5-94.ap-south-1.compute.internal>
2021-04-01 19:48:09 +0000

    added all

--- fact.c
+++ fact.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-  fact()
+  fact1()
 {
   int i, Number; 
   long Factorial = 1;
--- rev.c
+++ rev.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
  
-rev()
+rev1()
 {
   	char Str[100];
   	int i, len;
